67279ba920bbd067d9719f29d002c4ebb4b138f4
navruzoffd <navruzovdaniyal04@gmail.com>
2024-01-28 11:10:16 +0300

    make dao

---
+++ src/dao.py
@@ -0,0 +1,19 @@
+from src.database import async_session_maker
+from sqlalchemy import select
+from src.models import Users
+
+class UsersDAO:
+    @classmethod
+    async def get_users(cls):
+        async with async_session_maker() as session:
+            query = select(Users)
+            result = await session.execute(query)
+            return result.mappings().all()
+        
+    @classmethod
+    async def add_user(cls, user):
+        async with async_session_maker() as session:
+            query = Users(name=user.name, password=user.password)
+            session.add(query)
+            await session.commit()
+            return f"Пользователь {user.name} добавлен"
--- src/main.py
+++ src/main.py
@@ -1,23 +1,13 @@
 from fastapi import FastAPI
-from src.database import async_session_maker
-from sqlalchemy import select
-from src.models import Users
 from src.schemas import RegisterSheme
+from src.dao import UsersDAO
 
 app = FastAPI()
 
-@app.post("/registration")
-async def registration(user: RegisterSheme):
-    async with async_session_maker() as session:
-        query = Users(name=user.name, password=user.password)
-        session.add(query)
-        await session.commit()
-        return f'Пользователь {user.name} добавлен'
-
-
 @app.get("/users")
 async def get_users():
-    async with async_session_maker() as session:
-        query = select(Users)
-        result = await session.execute(query)
-        return result.scalars().all()
+    return await UsersDAO.get_users()
+
+@app.post("/registration")
+async def add_user(user: RegisterSheme):
+        return await UsersDAO.add_user(user)
